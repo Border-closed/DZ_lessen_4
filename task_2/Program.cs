897b53813530cab6f4ec1b5d57b04e7ad4a5025d
Dmitrii <directsdi@gmail.com>
2022-10-17 22:21:32 +0500

    сложение цифр от числа

---
+++ task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+Console.WriteLine("введите число:   ");
+string number = Console.ReadLine ();
+int L = number.Length;
+int result = 0;
+try 
+{
+for (int i = 0; i < L; i++)
+{
+    
+    result = result + int.Parse($"{number[i]}");
+}
+    System.Console.WriteLine(result);
+    }
+    catch
+    {
+        System.Console.WriteLine("Вы ввели не число  ");
+    }
